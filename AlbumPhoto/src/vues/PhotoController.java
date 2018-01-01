f0831f0ce328812ff42d9d1c846015c916d4afbb
DESKTOP-A26OD07\John <john.schmitt9@gmail.com>
2018-01-01 22:29:15 +0100

    Ajout d'une photo : fonctionnel

--- AlbumPhoto/src/vues/PhotoController.java
+++ AlbumPhoto/src/vues/PhotoController.java
@@ -1,18 +1,22 @@
 package vues;
 
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 //import java.net.URL;
-import java.nio.file.Files;
+//import java.nio.file.Files;
 //import java.nio.file.Path;
 //import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+//import java.nio.file.StandardCopyOption;
 //import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-//import javafx.scene.image.Image;
-//import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import modeles.Main;
@@ -26,7 +30,7 @@ public class PhotoController {
 	}
 	
 	@FXML
-	public void ajouterPhoto(){
+	public void ajouterPhoto() throws IOException{
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Ajouter une photo");
 		fileChooser.getExtensionFilters().addAll(
@@ -34,8 +38,15 @@ public class PhotoController {
 		File photo = fileChooser.showOpenDialog(null);
 		if(photo != null)
 		{
-			
-			try{
+			BufferedImage bufferedImage = ImageIO.read(photo);
+	        Image image1 = SwingFXUtils.toFXImage(bufferedImage, null);
+	        ImageView selectedImage = new ImageView(image1);  
+	        //selectedImage.setImage(image1);
+
+	        main.getRoot().getChildren().addAll(selectedImage);
+
+	       
+			/*try{
 				File destination = new File(this.main.getClass().getResource("").toString());
 				Files.copy(photo.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException ex) {
@@ -47,7 +58,7 @@ public class PhotoController {
 	        Image image = new Image(imageURL.toExternalForm()); 
 	        ImageView imageView = new ImageView(image); 
 	        this.main.getRoot().getChildren().setAll(imageView);*/
-			System.out.println(photo);
+			System.out.println("file://" + photo.toString());
 		}
 		System.out.println("Image ajoutée !");
 	}
