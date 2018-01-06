17fe4e3578acf92eab7a3af8968748461cf13ad8
Cindy <Cindy@DESKTOP-J40COIF>
2018-01-06 16:10:32 +0100

    Ajout du choix de la couleur et de la taille pour le cadre

--- AlbumPhoto/src/controllers/PhotoController.java
+++ AlbumPhoto/src/controllers/PhotoController.java
@@ -28,7 +28,7 @@ public class PhotoController {
 	private PhotoSimple photo;
 
 	public PhotoController(PhotoSimple photo) {
-		
+
 		this.setPhoto(photo);
 		BufferedImage bufferedImage;
 		try {
@@ -38,28 +38,27 @@ public class PhotoController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		this.setBordure(false);
-		
+
 		this.setSelectedImage(new ImageView(this.getImage()));
 		this.getSelectedImage().setPreserveRatio(true);
 		this.getSelectedImage().setFitWidth(300);
 		this.getSelectedImage().fitWidthProperty();
-		
+
 		this.setWidth(300);
-		this.setHeight((this.getWidth()/this.getImage().getWidth())*this.getImage().getHeight());
-		
+		this.setHeight((this.getWidth() / this.getImage().getWidth()) * this.getImage().getHeight());
+
 		this.setCadre(new Rectangle(this.getWidth(), this.getHeight()));
 		this.getCadre().setFill(Color.TRANSPARENT);
 		this.getCadre().setStroke(Color.TRANSPARENT);
 		this.getCadre().setStrokeWidth(0);
-		
-		//this.setCadre(new Rectangle(this.getWidth(), this.getHeight()));
-		//this.getCadre().setFill(Color.TRANSPARENT);
-		//this.getCadre().setStroke(Color.TRANSPARENT);
-		//this.getCadre().setStrokeWidth(0);
-		
-		
+
+		// this.setCadre(new Rectangle(this.getWidth(), this.getHeight()));
+		// this.getCadre().setFill(Color.TRANSPARENT);
+		// this.getCadre().setStroke(Color.TRANSPARENT);
+		// this.getCadre().setStrokeWidth(0);
+
 		this.setControleur(new Controller());
 	}
 
@@ -139,82 +138,131 @@ public class PhotoController {
 		this.image = image;
 	}
 
-	public void ajouterCadre()
-	{
-		if(this.isBordure()){
+	public void ajouterCadre() {
+		if (this.isBordure()) {
 			this.getCadre().setFill(Color.TRANSPARENT);
 			this.getCadre().setStroke(Color.BLUE);
 			this.getCadre().setStrokeWidth(10);
-			this.getCadre().relocate(this.getSelectedImage().getX()-this.getCadre().getStrokeWidth()/2, this.getSelectedImage().getY()-this.getCadre().getStrokeWidth()/2);}
+			this.getCadre().relocate(this.getSelectedImage().getX() - this.getCadre().getStrokeWidth() / 2,
+					this.getSelectedImage().getY() - this.getCadre().getStrokeWidth() / 2);
+		}
 
 	}
-	
-	public void supprimerCadre()
-	{
+
+	public void supprimerCadre() {
 		this.getCadre().setFill(Color.TRANSPARENT);
 		this.getCadre().setStroke(Color.TRANSPARENT);
 		this.getCadre().setStrokeWidth(0);
-		this.getCadre().relocate(this.getSelectedImage().getX()-this.getCadre().getStrokeWidth()/2, this.getSelectedImage().getY()-this.getCadre().getStrokeWidth()/2);
+		this.getCadre().relocate(this.getSelectedImage().getX() - this.getCadre().getStrokeWidth() / 2,
+				this.getSelectedImage().getY() - this.getCadre().getStrokeWidth() / 2);
 	}
-	
-	
+
+	public void modifierCouleurCadre(String color) {
+		Color tmp = null;
+		switch (color) {
+		case "AQUA":
+			tmp = Color.AQUA;
+			break;
+		case "BLACK":
+			tmp = Color.BLACK;
+			break;
+		case "BLUE":
+			tmp = Color.BLUE;
+			break;
+		case "BLUEVIOLET":
+			tmp = Color.BLUEVIOLET;
+			break;
+		case "BROWN":
+			tmp = Color.BROWN;
+			break;
+		case "CYAN":
+			tmp = Color.CYAN;
+			break;
+		case "DARKBLUE":
+			tmp = Color.DARKBLUE;
+			break;
+		case "GREEN":
+			tmp = Color.GREEN;
+			break;
+		case "RED":
+			tmp = Color.RED;
+			break;
+		case "YELLOW":
+			tmp = Color.YELLOW;
+			break;
+		case "TEAL":
+			tmp = Color.TEAL;
+			break;
+		}
+		this.getCadre().setStroke(tmp);
+	}
+
 	public void drawWithJavaFX(Init init) {
-		
+
 		// afficher l'image
 		this.getSelectedImage().setX(this.getPhoto().getX());
 		this.getSelectedImage().setY(this.getPhoto().getY());
-		
-		// afficher la légende
-		this.setT(new Text (this.getSelectedImage().getX()+20, this.getSelectedImage().getY()+this.getHeight()+20, this.getPhoto().getTitre()));
-        
-        // définir un cadre
-        //this.ajouterCadre(true);
-        // ajouter un effet de profondeur
-        /*DropShadow borderGlow= new DropShadow();
-		borderGlow.setOffsetY(0f);
-		borderGlow.setOffsetX(0f);
-		borderGlow.setColor(Color.RED);
-		borderGlow.setWidth(80);
-		borderGlow.setHeight(80);
-		this.getCadre().setEffect(borderGlow);
-		*/
-		
-                
-        this.getPhoto().setX(this.getSelectedImage().getX());
-        this.getPhoto().setY(this.getSelectedImage().getY());
-        
-        // Event pour permettre à l'utilisateur de modifier la légende
-        this.getT().addEventHandler(MouseEvent.MOUSE_CLICKED, event-> {
-        	/* TextInput pour laisser l'utiliateur modifier le titre de la photo */
-    		TextInputDialog input_titre = new TextInputDialog(this.getT().getText());
-    		input_titre.setTitle("Légende de la photo");
-    		input_titre.setContentText("Légende de la photo : ");
-    		input_titre.setHeaderText(null);
-    		
-    		// Réponse de l'utilisateur
-    		Optional<String> result = input_titre.showAndWait();
-    		if (result.isPresent()){
-    			this.getT().setText(result.get());
-    		}
-        });
-                
-        this.getSelectedImage().addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
-        	this.getSelectedImage().setX(event.getX()-(this.getWidth()/2));
-        	this.getSelectedImage().setY(event.getY()-(this.getHeight()/2));
-        	this.getT().setX(this.getSelectedImage().getX()+20);
-        	this.getT().setY(this.getSelectedImage().getY()+this.getHeight()+20);
-        	this.getCadre().relocate(this.getSelectedImage().getX()-this.getCadre().getStrokeWidth()/2, this.getSelectedImage().getY()-this.getCadre().getStrokeWidth()/2);
-				event.consume();
-	    });
-                
-        this.getSelectedImage().addEventHandler(MouseEvent.MOUSE_CLICKED, event-> {
-        	init.getToolbar().init(this, init);
-        	event.consume();
-        });
 
-        //this.setCadre(true);
-        init.getRoot().getChildren().addAll(this.getT(),this.getCadre(),this.getSelectedImage());
+		// afficher la légende
+		this.setT(new Text(this.getSelectedImage().getX() + 20, this.getSelectedImage().getY() + this.getHeight() + 20,
+				this.getPhoto().getTitre()));
+
+		// définir un cadre
+		// this.ajouterCadre(true);
+		// ajouter un effet de profondeur
+		/*
+		 * DropShadow borderGlow= new DropShadow(); borderGlow.setOffsetY(0f);
+		 * borderGlow.setOffsetX(0f); borderGlow.setColor(Color.RED);
+		 * borderGlow.setWidth(80); borderGlow.setHeight(80);
+		 * this.getCadre().setEffect(borderGlow);
+		 */
+
+		this.getPhoto().setX(this.getSelectedImage().getX());
+		this.getPhoto().setY(this.getSelectedImage().getY());
+
+		// Event pour permettre à l'utilisateur de modifier la légende
+		this.getT().addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			/*
+			 * TextInput pour laisser l'utiliateur modifier le titre de la photo
+			 */
+			TextInputDialog input_titre = new TextInputDialog(this.getT().getText());
+			input_titre.setTitle("Légende de la photo");
+			input_titre.setContentText("Légende de la photo : ");
+			input_titre.setHeaderText(null);
+
+			// Réponse de l'utilisateur
+			Optional<String> result = input_titre.showAndWait();
+			if (result.isPresent()) {
+				this.getT().setText(result.get());
+			}
+		});
+
+		this.getSelectedImage().addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
+			this.getSelectedImage().setX(event.getX() - (this.getWidth() / 2));
+			this.getSelectedImage().setY(event.getY() - (this.getHeight() / 2));
+			this.getT().setX(this.getSelectedImage().getX() + 20);
+			this.getT().setY(this.getSelectedImage().getY() + this.getHeight() + 20);
+			this.getCadre().relocate(this.getSelectedImage().getX() - this.getCadre().getStrokeWidth() / 2,
+					this.getSelectedImage().getY() - this.getCadre().getStrokeWidth() / 2);
+			event.consume();
+		});
+
+		this.getSelectedImage().addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			init.getToolbar().init(this, init);
+			event.consume();
+		});
+
+		// this.setCadre(true);
+		init.getRoot().getChildren().addAll(this.getT(), this.getCadre(), this.getSelectedImage());
 	}
-	
-	
+
+	public void modifierTailleCadre(String newValue) {
+		try {
+			this.getCadre().setStrokeWidth(Integer.parseInt(newValue));
+		} catch (Exception e) {
+
+		}
+
+	}
+
 }
--- AlbumPhoto/src/controllers/ToolBarController.java
+++ AlbumPhoto/src/controllers/ToolBarController.java
@@ -2,17 +2,26 @@ package controllers;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class ToolBarController {
 	private PhotoController photo;
 	private Init init;
 	private CheckBox checkbox;
 	private Boolean ignore;
+	private ChoiceBox<String> choiceBoxCouleur;
+	private TextField tailleCadre;
 
 	public ToolBarController() {
 		super();
 		this.ignore = false;
+
 	}
 
 	public void afficherToolBar() {
@@ -24,10 +33,7 @@ public class ToolBarController {
 		this.init = init;
 		this.photo = photoController;
 		afficherToolBar();
-		this.checkbox = (CheckBox) init.getRoot().lookup("#checkbox_bordure");
-
-		cocher();
-
+		this.checkbox = (CheckBox) init.getRoot().lookup("#checkbox_cadre");
 		this.checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
 			public void changed(ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) {
 				verif();
@@ -37,6 +43,39 @@ public class ToolBarController {
 			}
 
 		});
+		
+		this.choiceBoxCouleur = (ChoiceBox) init.getRoot().lookup("#choicebox_couleur_cadre");
+		this.choiceBoxCouleur.getSelectionModel()
+	    .selectedItemProperty()
+	    .addListener( (ObservableValue<? extends String> observable, String oldValue, String newValue) -> modifierCouleurCadre(newValue) );
+
+		
+		this.tailleCadre = (TextField) init.getRoot().lookup("#textField_taille");
+		this.tailleCadre.textProperty().addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> observable,
+		            String oldValue, String newValue) {
+		    	if (newValue.matches("\\d*")) {
+		    		modifierTailleCadre(newValue);
+		    	}
+		    }
+		});
+		
+		
+		cocher();
+
+		
+	}
+
+	protected void modifierTailleCadre(String newValue) {
+		photo.modifierTailleCadre(newValue);
+		
+	}
+
+	public void modifierCouleurCadre(String newValue) {
+		if(photo.isBordure()) {
+			photo.modifierCouleurCadre(newValue);
+		}
 	}
 
 	// Cette fonction permet de mettre à jour la Checkbox en fonction de l'image
@@ -47,8 +86,12 @@ public class ToolBarController {
 
 		if (photo.isBordure()) {
 			checkbox.setSelected(true);
+			choiceBoxCouleur.setDisable(false);
+			tailleCadre.setDisable(false);
 		} else {
 			checkbox.setSelected(false);
+			choiceBoxCouleur.setDisable(true);
+			tailleCadre.setDisable(true);
 		}
 	}
 
@@ -56,10 +99,14 @@ public class ToolBarController {
 		if (checkbox.isSelected()) {
 			photo.setBordure(true);
 			photo.ajouterCadre();
+			choiceBoxCouleur.setDisable(false);
+			tailleCadre.setDisable(false);
 		} else {
 			photo.setBordure(false);
 			photo.supprimerCadre();
 			checkbox.setSelected(false);
+			choiceBoxCouleur.setDisable(true);
+			tailleCadre.setDisable(true);
 		}
 	}
 
